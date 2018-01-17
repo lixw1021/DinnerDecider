d113bf02fbdf03adcb6f764ea06e985514ed6be5
xianwei li <lixw1021@gmail.com>
2018-01-17 10:26:39 -0800

    implement add and decide funtions

--- app/src/main/java/com/xianwei/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/xianwei/dinnerdecider/MainActivity.kt
@@ -2,11 +2,26 @@ package com.xianwei.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var foodArray = arrayListOf("Chinese", "Hamburger", "pizza", "Starbucks")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_decider.setOnClickListener {
+            val index = Random().nextInt(foodArray.count())
+            tv_food_name.setText(foodArray[index])
+        }
+
+        btn_add_food.setOnClickListener {
+            val newFood = et_add_food.text.toString()
+            foodArray.add(newFood)
+            et_add_food.text.clear()
+        }
     }
 }
